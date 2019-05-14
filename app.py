66b36bcc80d325aafb62e883d29cd3598fc6c6ec
Sultan Iman <sultan.imanhodjaev@gmail.com>
2019-05-14 12:53:50 +0200

    chore: allow background counter sync

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import uvicorn
 
 from threading import Thread
-
 from core import app, transport
-from core.views import merge_state
+from core.views import merge_state, sync
 
 
 if __name__ == '__main__':
     Thread(target=transport.listen, args=(merge_state,)).start()
+    Thread(target=sync).start()
     uvicorn.run(app, host='0.0.0.0', port=8000)
--- core/views.py
+++ core/views.py
@@ -1,3 +1,5 @@
+import time
+
 from starlette.responses import JSONResponse
 from core import app, grow_counter, may_be_update, ViewCounter
 
@@ -5,15 +7,20 @@ from core import app, grow_counter, may_be_update, ViewCounter
 view_counter = ViewCounter()
 
 
-def merge_state(updates):
-    view_counter.merge(updates)
-
-
 @app.route('/my-video')
 async def homepage(_request):
     view_counter.increment()
-    may_be_update(view_counter)
 
     return JSONResponse({
         'view_count': grow_counter.value(view_counter.state)
     })
+
+
+def merge_state(updates):
+    view_counter.merge(updates)
+
+
+def sync():
+    while True:
+        time.sleep(1)
+        may_be_update(view_counter)
